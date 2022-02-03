5e1fb633fd92c7495f119d77f79aba28241380b9
suti1979 <suti1979@gmail.com>
2022-02-03 22:22:05 +0100

    constructor fill

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) {
-        Scenery scenery = new Scenery(4);
+        Scenery scenery = new Scenery(5);
         scenery.displayWorld();
         scenery.foundSpring();
-        scenery.displayWorld();
+
     }
 }
--- src/Scenery.java
+++ src/Scenery.java
@@ -5,6 +5,19 @@ public class Scenery {
     public Scenery(int n) {
         size = n;
         world = new int[n][n];
+
+        // int previousCell = getRandomNumber(1, size);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                world[i][j] = getRandomNumber(1, size);
+
+            }
+        }
+    }
+
+    private int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max + 1 - min)) + min);
     }
 
     public void displayWorld() {
@@ -18,7 +31,8 @@ public class Scenery {
     }
 
     public void foundSpring() {
-        world[0][0] = -1;
+        world[0][0] = -world[0][0];
+        displayWorld();
     }
 
 }
