547a38fb243b9da7daa1c01d8ae20b8e081b3879
suti1979 <suti1979@gmail.com>
2022-02-04 19:22:05 +0100

    found Spring mapping

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) {
-        Scenery scenery = new Scenery(5);
+        Scenery scenery = new Scenery(4);
         scenery.displayWorld();
         scenery.foundSpring();
-
+        // scenery.floodTheValley();
     }
 }
--- src/Scenery.java
+++ src/Scenery.java
@@ -1,38 +1,60 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Scenery {
     private int[][] world;
     private int size;
+    private Map<Integer, Integer> mapTiles = new HashMap<>();
 
     public Scenery(int n) {
         size = n;
         world = new int[n][n];
-
-        // int previousCell = getRandomNumber(1, size);
+        int actualTile;
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                world[i][j] = getRandomNumber(1, size);
+                actualTile = getRandomNumber(1, 4);
+                world[i][j] = actualTile;
+
+                if (!mapTiles.containsKey(actualTile)) {
+                    mapTiles.put(actualTile, 1);
+                } else {
+                    mapTiles.put(actualTile, mapTiles.get(actualTile) + 1);
+                }
 
             }
         }
     }
 
-    private int getRandomNumber(int min, int max) {
-        return (int) ((Math.random() * (max + 1 - min)) + min);
+    public void foundSpring() {
+        int smallestTerain = Collections.min(mapTiles.keySet());
+        int numberOfsmallestTerrain = mapTiles.get(smallestTerain);
+        numberOfsmallestTerrain = getRandomNumber(1, numberOfsmallestTerrain);
+        // world[0][0] = -world[0][0];
+        displayWorld();
+    }
+
+    public void floodTheValley() {
+        displayWorld();
     }
 
     public void displayWorld() {
+        int geoData;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                System.out.print(world[i][j] + " ");
+                geoData = world[i][j];
+                if (geoData > 0)
+                    System.out.print(" ");
+                System.out.print(geoData);
             }
             System.out.println();
         }
         System.out.println();
     }
 
-    public void foundSpring() {
-        world[0][0] = -world[0][0];
-        displayWorld();
+    private int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max + 1 - min)) + min);
     }
 
 }
